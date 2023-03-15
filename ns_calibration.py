570678f3b1b8b81c2cb317011129ad524b848831
Volodymyr <83507366+volom@users.noreply.github.com>
2023-03-15 17:54:08 +0200

    update ns model for 4 parameters

--- ns_calibration.py
+++ ns_calibration.py
@@ -18,3 +18,28 @@ params, _ = curve_fit(svensson, maturity, yields, p0)
 # Print the calibrated parameters
 
 print(params)
+
+
+
+# 4 parameters
+
+from scipy.optimize import curve_fit
+import numpy as np
+
+# Define the Svensson function
+def svensson(term, beta0: float, beta1: float, beta2: float, tau: float)  -> float:
+    return beta0 + beta1 * ((1 - math.exp(-term / tau)) / (term / tau)) + beta2 * (((1 - math.exp(-term / tau)) / (term / tau)) - math.exp(-term / tau))
+
+# Generate some sample data
+maturity = np.array([1, 2, 3, 5, 7, 10, 20, 30])
+yields = np.array([0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09])
+
+# Define the initial values for the parameters
+p0 = [0.02, 0.03, 0.04, 0.05]
+
+# Calibrate the model
+params, _ = curve_fit(svensson, maturity, yields, p0)
+
+# Print the calibrated parameters
+
+print(params)
